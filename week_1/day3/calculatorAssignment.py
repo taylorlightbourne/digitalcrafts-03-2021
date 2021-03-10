bd82baff3520bc61a4edccc188e1e9278208e5df
Taylor Lightbourne <taylorlightbourne@gmail.com>
2021-03-10 18:25:33 -0500

    Assignment 2 Complete and Changes Made to Assignment 1

--- week_1/day3/calculatorAssignment.py
+++ week_1/day3/calculatorAssignment.py
@@ -1,6 +1,19 @@
+# Assignment 1
 num1 = input("Enter the first number: ")
+lengthOfNum1 = len(num1)
+while(lengthOfNum1 < 1):
+    num1 = input("Enter the first number: ")
+    lengthOfNum1 = len(num1)
 operator = input("Enter the operation as +, -, *, or /: ")
+lengthOfOperator = len(operator)
+while(lengthOfOperator < 1):
+    operator = input("Enter the operation as +, -, *, or /: ")
+    lengthOfOperator = len(operator)
 num2 = input("Enter the second number: ")
+lengthOfNum2 = len(num2)
+while(lengthOfNum2 < 1):
+    num2 = input("Enter the second number: ")
+    lengthOfNum2 = len(num2)
 if operator == "+":
     answer = float(num1) + float(num2)
     print(answer)
@@ -13,3 +26,11 @@ if operator == "*":
 if operator == "/":
     answer = float(num1) / float(num2)
     print(answer)
+
+
+# Assignment 2
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+    print("{0} is Even". format(num))
+else:
+    print("{0} is Odd". format(num))
