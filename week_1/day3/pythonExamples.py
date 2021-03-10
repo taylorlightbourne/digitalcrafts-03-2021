c33be7a0dba2ff910fa665a8233b9dcbf04bb43c
Taylor Lightbourne <taylorlightbourne@gmail.com>
2021-03-10 12:35:34 -0500

    added class size

--- week_1/day3/pythonExamples.py
+++ week_1/day3/pythonExamples.py
@@ -1,16 +1,19 @@
-# # Variable
-# # String === "", "word", "1", "functionName"
+# Variable
+# nameOfVaribale = assignedValueToVariable
+# String === "", "word", "1", "functionName"
 greeting = "Hello World"
-# # Integer === whole number, 1, 23, 934234, 9392
+# Integer === whole number, 1, 23, 934234, 9392
 classSize = 23
-# # Float === decimal, 4.5, 4.5565565, 4.21564151541564154515465
+dirPlusTeacher = 4
+# Float === decimal, 4.5, 4.5565565, 4.21564151541564154515465
 floatExample = 2.323
-# # Boolean === true or false, true, false, returns a truthy or falsy value
-sumOfNumbers = 1 + 1
-# # Name, assignment operator (=), value that I am assigning
+# Boolean === true or false, true, false, returns a truthy or falsy value
+sumOfClass = dirPlusTeacher + classSize
+# Name, assignment operator (=), value that I am assigning
 variableName = True
 somethingToCallIt = ["students", 23, True]
 somethingToCallIt = {"helloPythonWorld": 23, "we are students": 24.5}
+print("The size of our class is", sumOfClass)
 # # List, sequence of values followed by [value1,value2,value3]
 # [1,2,3],
 # ["strings","stings2","string3"]
@@ -21,10 +24,10 @@ somethingToCallIt = {"helloPythonWorld": 23, "we are students": 24.5}
 # # Dictionary 
 # # Sequence of labeled values {key: values} 
 # {"stringName": "stringValue"}
-# {"favoriteColor": "Red"}
-
-print("Hello World")
-print("Welcome to DigitalCrafts Bootcamp")
-print(classSize)
-print(somethingToCallIt)
-print(somethingToCallIt)
+# # {"favoriteColor": "Red"}
+# print("Hello World")
+# print("Welcome to DigitalCrafts Bootcamp")
+# print(classSize)
+# print(somethingToCallIt)
+# print(somethingToCallIt)
+# variableNameThree = 3
