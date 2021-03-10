76cc7fcf2b009610821237617207fb423bd96414
Taylor Lightbourne <taylorlightbourne@gmail.com>
2021-03-10 11:24:48 -0500

    created my first program

---
+++ week_1/day3/pythonExamples.py
@@ -0,0 +1,24 @@
+# # Variable
+# # String === "", "word", "1", "functionName"
+# greeting = "Hello World"
+# # Integer === whole number, 1, 23, 934234, 9392
+# classSize = 23
+# # Float === decimal, 4.5, 4.5565565, 4.21564151541564154515465
+# floatExample = 2.323
+# # Boolean === true or false, true, false, returns a truthy or falsy value
+# sumOfNumbers = 1 + 1
+# # Name, assignment operator (=), value that I am assigning
+# variableName = True
+# # List, sequence of values followed by [value1,value2,value3]
+# [1,2,3],
+# ["strings","stings2","string3"]
+# [False, True, False]
+# ["string", 1, 3.4, True, "string"]
+# # Nesting
+# ["list", ["list"]] # List inside of a list 
+# # Dictionary 
+# # Sequence of labeled values {key: values} 
+# {"stringName": "stringValue"}
+# {"favoriteColor": "Red"}
+
+print("Hello World")
