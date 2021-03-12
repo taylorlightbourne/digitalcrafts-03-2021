efd5b7867b55a10ce549ff2d0fd4a10190e8e002
Taylor Lightbourne <taylorlightbourne@gmail.com>
2021-03-11 19:07:59 -0500

    entered joe's code

--- week_1/day4/calculatorAssignment.py
+++ week_1/day4/calculatorAssignment.py
@@ -1,35 +1,37 @@
 # Assignment 1
 # FUNCTIONS
 
-num1 = int(input("Enter the first number: "))
-operation = input("Enter the operation as +, -, *, or /: ")
-num2 = int(input("Enter the second number: "))
-currentInput = 
-
-def add(num1, num2):
-    add = (num1 + num2)
-    return print(add)
-
-
-def subtract(num1, num2):
-    subtract = (num1 - num2)
-    return print(subtract)
-
-def multiply(num1, num2):
-    multiply = (num1 * num2)
-    return print(multiply)
-
-def divide(num1, num2):
-    divide = (num1 / num2)
-    return print(divide)
-
-if operation == "+":
-    add(num1, num2)
-elif operation == "-":
-    subtract(num1, num2)
-elif operation == "*":
-    multiply(num1, num2)
-elif operation == "/":
-    divide(num1, num2)
-
-
+def welcomeMessage():
+    message = """\n \n \n 
+    \n \n \n 
+    "Welcome to a calculator!"
+    ************************
+    ************************
+    \n \n \n 
+    """
+    return print(message)
+def operation(operator, number1, number2):
+    answer = ""
+    if(operator == "+"):
+        answer = int(number1) + int(number2)
+    elif(operator == "*"):
+        answer = int(number1) * int(number2)
+    elif(operator == "/"):
+        answer = int(number1) / int(number2)
+    elif(operator == "-"):
+        answer = int(number1) - int(number2)
+    else:
+        print("Operator invalid")
+    return print("Your operation of %s %s %s is = %s \n" % (number1, operator, number2, answer))
+choice = ""
+while(choice != "y"):
+    welcomeMessage()
+    firstNumber = input("Please enter in your first number\n")
+    operand = input(
+        "Please enter in your choice of operation, ( +, - , * , / )\n")
+    secondNumer = input("Please enter in your second number\n")
+    operation(operand, firstNumber, secondNumer)
+    quitMessage = input("Would you like to quit? y (yes) or n (no)")
+    choice = quitMessage
+# quitKey = input("")
+# while quitKey != "q":
