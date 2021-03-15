f75ced8c792f12648d09a0f59c9ec5ee36b4c145
Taylor Lightbourne <taylorlightbourne@gmail.com>
2021-03-15 10:17:16 -0400

    save changes

--- week_1/day4/homework.py
+++ week_1/day4/homework.py
@@ -2,7 +2,17 @@ print("****TODO APP*****\n" \
     "Please select an option from the drop down menu.\n" \
     "Enter 1 to add task\n" \
     "Enter 2 to delete task\n" \
-    "Enter 3 to view all tasks\n")
+    "Enter 3 to view all tasks\n" \
+    "Press 'q' to quit application\n")
+
+    # return print(message)
+
+# choice = ""
+# while(choice != "q"):
+#     welcomeMessage()
+#     userChoices = input("Select 1, 2, or 3 from list above: ")
+#     outcome = determineTask(userChoices)
+#     choice= outcome
 
 select = int(input("Select 1, 2, or 3 from list above: ")) 
 
@@ -14,3 +24,4 @@ if select == 1:
 # elif select == 2:
 #     list = 
 #     print(list)
+# 
