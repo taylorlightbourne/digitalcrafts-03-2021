c448ec59201384d411f4232b43794c3c6d53d823
Taylor Lightbourne <taylorlightbourne@gmail.com>
2021-03-16 09:47:02 -0400

    completed with functions

--- week_1/day4/homework.py
+++ week_1/day4/homework.py
@@ -1,27 +1,69 @@
-print("****TODO APP*****\n" \
-    "Please select an option from the drop down menu.\n" \
-    "Enter 1 to add task\n" \
-    "Enter 2 to delete task\n" \
-    "Enter 3 to view all tasks\n" \
-    "Press 'q' to quit application\n")
+def welcomeMessage():
+    message = ("""
+    ****TODO APP*****\n
+    Please select an option below:\n
+    Enter 1 to add task\n
+    Enter 2 to delete task\n 
+    Enter 3 to view all tasks\n 
+    Press 'q' to quit application\n
+    """)
 
-    # return print(message)
+    return print(message)
 
-# choice = ""
-# while(choice != "q"):
-#     welcomeMessage()
-#     userChoices = input("Select 1, 2, or 3 from list above: ")
-#     outcome = determineTask(userChoices)
-#     choice= outcome
+def quitMessage():
+    print("Goodbye, see you next time!")
+    return "q"
 
-select = int(input("Select 1, 2, or 3 from list above: ")) 
 
-if select == 1:
-    taskAnswer = input("What is the task? ")
-    priorityAnswer = input("What is the priority of the task? \n" \
-    "High, Medium, or Low\n")
-    print(taskAnswer + "-" + priorityAnswer) 
-# elif select == 2:
-#     list = 
-#     print(list)
-# 
+def addTodo():
+    taskToAdd = input("What task would you like to add?\n")
+    taskPriority = input("What priority is this?\n high, medium, low?\n")
+    todoDictionary["title"] = taskToAdd
+    todoDictionary["priority"] = taskPriority
+    todoList.append(todoDictionary)
+    return print("Added * %s * to your todo list" % taskToAdd)
+
+
+def delTodo():
+    printTodo()
+    taskToDelete = input("What task would you like to delete?\n")
+    taskIndex = int(taskToDelete) - 1
+    taskToBeDeleted = todoList[taskIndex]
+    del todoList[taskIndex]
+    return print("Deleted * %s * from your todo list" % taskToBeDeleted["title"])
+
+
+def printTodo():
+    count = 1
+    print("Here is your todo list\n")
+    print("========================")
+    for task in todoList:
+        print("  %d. %s - %s" % (count, task["title"], task["priority"]))
+        count += 1
+    print("========================")
+    return "continue"
+
+
+def determineTask(userChoice):
+    outcome = ""
+    if(userChoice == "1"):
+        outcome = addTodo()
+    elif(userChoice == "2"):
+        outcome = delTodo()
+    elif(userChoice == "3"):
+        outcome = printTodo()
+    elif(userChoice == "q"):
+        outcome = quitMessage()
+    else:
+        print("Choose something or hit q to quit.")
+
+    return outcome
+
+choice = ""
+
+while(choice != "q"):
+    welcomeMessage()
+    usersChoice = input("What would you like to do?\n")
+    todoList = []
+    outcome = determineTask(usersChoice)
+    choice = outcome
