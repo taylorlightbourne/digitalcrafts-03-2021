c59f94f01120b2d3ef6f62932a594bbf81cb076d
Taylor Lightbourne <taylorlightbourne@gmail.com>
2021-03-17 10:01:43 -0400

    edits

--- week_2/day2/assignmentPractice.py
+++ week_2/day2/assignmentPractice.py
@@ -32,7 +32,7 @@ Press "q" to quit
 
 def addPermUser():
     permNameToAdd = input("What is your name? ")
-    permAgeToAdd = input("What is your age? ")
+    permAgeToAdd = int(input("What is your age? "))
     return print("Added * %s * as permanent user" % permNameToAdd)
 
 def addTempUser():
@@ -57,3 +57,24 @@ while(choice != "q"):
     break
     
 
+# 2
+# Create a building class
+# buildilng class will have
+# height
+# capacity
+# sqft
+# # commercial or residential
+# commercial
+
+# ask the user to create a building (new instance of the building class)
+# they will enter in height
+# capacity (people)
+# sqft
+
+# 5 instances of a building
+# print out the specs of the buildilng everytime a building is made
+
+# print statement needs to be a method
+
+
+# input = int(input("What's your name?"))
--- week_2/day2/practiceRunGame.py
+++ week_2/day2/practiceRunGame.py
@@ -45,4 +45,114 @@ def characterCreation():
         print("Please enter a valid choice.")
     return character
 
+# You have two characters, a Hero and a Goblin
+# you goal is to let them fight, whoever wins, will be printed out
+# in a victory message (fancy way of saying a function that prints)
+# whoever is defeated can be printed out in that message as well
+
+# Hero
+# name
+# attack
+# defense
+# hp
+
+# Goblin
+# name
+# attack
+# defense
+# hp
+# clan
+
+
+class Hero:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+        self.characterClass = "Hero"
+
+    def takeDamage(self):
+        self.hp -= 5
+
+    class SwagMan:
+        def __init__(self, name):
+            self.name = name
+
+
+class Goblin:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+        self.characterClass = "Goblin"
+
+    def takeDamage(self):
+        self.hp -= 5
+
+
+class Knight:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+        self.characterClass = "Knight"
+
+
+def characterAttack(char1, char2):
+
+    print(f"{char1.name} attacks {char2.name}")
+    char2.takeDamage()
+    print(f"{char2.name} takes 5 damage")
+    print(f"{char2.name} has {char2.hp} left")
+
+
+def victoryMessage(hero, goblin):
+    print(f"{hero.name} is the victor and they have slain {goblin.name}")
+
+
+def characterCreation():
+    character = ""
+    charName = input("What is your name?")
+    charClass = input(
+        "What is your class? Choose exactly ((k)Knight, (g)Goblin, (h)Hero)")
+
+    if charClass == "h":
+        character = Hero(charName, 10)
+    elif charClass == "g":
+        character = Goblin(charName, 10)
+    elif charClass == "k":
+        character = Knight(charName, 10)
+    else:
+        print("Please follow my rules and enter a valid choice")
+    return character
+
+
+joe = Goblin("Joe", 10)
+philip = Hero("Phillip", 10)
+whitney = Hero("Whitney", 10)
+randi = Hero("Randi", 10)
+randy = Goblin("Randy", 10)
+carlo = Knight("Carlo", 10)
+
+# print("the name of this character", joe.name)
+# print("the name of this character", philip.name)
+# print("the name of this character", whitney.name)
+# print("the name of this character", randi.name)
+# print("the name of this character", randy.name)
+# print("the name of this character", carlo.name)
+
+
+# randi to punch randy, simulating your character attacking another character
+
+# you take 5 damage to your health
+# I ask the user to type in their name
+# I give them health values
+# I print out their health values
+# I then set a global variable to be the while loop condition
+# character = characterCreation()
+# print(character.name, character.hp, character.characterClass)
+
+# while character.hp > 0:
+#     print("What would you like to do")
+#     if()
+# characterAttack(randi, randy)
+# if randy.hp == 0:
+#     victoryMessage(carlo, joe)
     
