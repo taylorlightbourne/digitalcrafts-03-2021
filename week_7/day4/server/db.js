9539a86a703c94fc43bae0e7bb4ce260a6fab7bb
Taylor Lightbourne <79942688+taylorlightbourne@users.noreply.github.com>
2021-04-22 16:49:41 -0400

    removed password

--- week_7/day4/server/db.js
+++ week_7/day4/server/db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "Righteous!1",
+  password: "",
   host: "localhost",
   port: 5432,
   database: "productlist",
